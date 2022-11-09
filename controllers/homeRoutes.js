87b74a8ec8c66d4fb76a269846d1b1bd84d13374
Jonathan Moreno <johnfrom209@gmail.com>
2022-11-09 14:35:22 -0800

    working on the api request (not working yet)

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,11 +1,53 @@
+const { application } = require('express');
+const { json } = require('sequelize');
+const fetch = require('node-fetch');
+const { default: axios } = require('axios');
+const axios = require('axios').default;
+
 const router = require('express').Router();
+require('dotenv').config();
+
 
 
 router.get('/', async (req, res) => {
 
-    res.render('homepage', {
-        // pass the data to handlebars
-    })
+    let requestURL = `https://api.rawg.io/api/games?key=${process.env.API_KEY}`;
+
+    axios.get(requestURL)
+        .then(function (response) {
+            console.log("results: " + bigData.map((data) => data.get({ plain: true })));
+
+            const idkwhatthisis = results.map((game) => game.get({ plain: true }));
+
+            res.render('homepage', {
+                // pass the data to handlebars
+                idkwhatthisis
+            })
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+
+    // try {
+
+
+    //     // let results = await fetch(requestURL);
+
+    //     // const bigData = await results.json();
+
+    //     // console.log("results: " + bigData.map((data) => data.get({ plain: true })));
+
+    //     // const idkwhatthisis = results.map((game) => game.get({ plain: true }));
+
+    //     res.render('homepage', {
+    //         // pass the data to handlebars
+    //         // idkwhatthisis
+
+    //     })
+
+    // } catch (err) {
+    //     res.status(500).json(err)
+    // }
 
 })
 
--- public/js/apiFetch.js
+++ public/js/apiFetch.js
@@ -2,7 +2,15 @@ const router = require('express').Router();
 require('dotenv').config();
 
 
-async function getApiRawg (games) {
+async function getApiRawg() {
+    console.log("Inside apiRawg");
+
+    let requestURL = 'https://api.rawg.io/api/games?key=' + proccess.env.API_KEY;
+
     let res = await fetch(`https://api.rawg.io/api/games?key=${proccess.env.API_KEY}`);
     let rawgData = await res.json()
+    console.log(rawgData);
+    return rawgData;
 }
+
+getApiRawg();
