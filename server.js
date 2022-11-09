8f3ea12d4268e3a3d984d6d9e066cd9957a1e3a2
Jonathan Moreno <johnfrom209@gmail.com>
2022-11-09 11:48:50 -0800

    adding over the server.js file (may need to edit)

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const path = require('path');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sequelize = require('./config/connection');
+const routes = require('./Controller/routes');
+const helpers = require('./utils/helpers');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {
+        maxAge: 120 * 100000,
+        httpOnly: false,
+        secure: false,
+        sameSite: 'strict'
+    },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize,
+    }),
+};
+
+app.use(session(sess));
+
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+});
