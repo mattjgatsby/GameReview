3ec59d9c6baec5b2f1ca482a97923f1887cb1873
Gatsby <111303029+mattjgatsby@users.noreply.github.com>
2022-11-10 14:05:17 -0500

    added logout.js file in public js, formated with prettier

--- public/js/login.js
+++ public/js/login.js
@@ -21,30 +21,26 @@ const loginForm = async (event) => {
 };
 
 const signpForm = async (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    const userName = document.querySelector('#name-signup').value.trim()
-    const userPassword = document.querySelector('#password-signup').value.trim()
-    
-    if(userName && userPassword) {
-        const response = await fetch('/api/users', {
-            method: 'POST',
-            body: JSON.stringify({userName, userPassword}),
-            headers: { 'Content-Type': 'application/json'}
-        })
+  const userName = document.querySelector("#name-signup").value.trim();
+  const userPassword = document.querySelector("#password-signup").value.trim();
 
-        if(response.ok) {
-            document.location.replace('/profile');
-        } else {
-            alert(response.statusText)
-        }
+  if (userName && userPassword) {
+    const response = await fetch("/api/users", {
+      method: "POST",
+      body: JSON.stringify({ userName, userPassword }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location.replace("/profile");
+    } else {
+      alert(response.statusText);
     }
-}
+  }
+};
 
-document
-.querySelector('.login-form')
-.addEventListener('submit', loginForm)
+document.querySelector(".login-form").addEventListener("submit", loginForm);
 
-document
-.querySelector('.signup-form')
-.addEventListener('submit', signpForm)
+document.querySelector(".signup-form").addEventListener("submit", signpForm);
