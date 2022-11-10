dfbbddb65f92f548fb741a0c20f1467eacf69b48
wayne james <jackmcwilliams@Jacks-MacBook-Air.local>
2022-11-10 12:04:07 -0800

    game route

--- controllers/api/gameRoutes.js
+++ controllers/api/gameRoutes.js
@@ -1,19 +1,46 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const { Games, Review } = require("../../models");
 
+router.get("/", async (req, res) => {
+  // retro fit this code to our db
+  try {
+    const gamesData = await Games.findAll({
+      include: [{
+          model: Review,
+          attributes: ["id", "review_body", "review_date", "user_id"],
+        },
+      ],
+    });
+    console.log(gamesData
+        );const game = gamesData.map(games) => game.get({plain:true});
+    console.log("game", game);
+    res.status(200).json(gamesData);
+    console.log()
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
-router.get('/', async (req, res) => {
-
+router.get('/:id', async (req, res) => {
     // retro fit this code to our db
-    // try {
-    //     const userData = await User.findAll({
-    //         include: [{ model: Post }, { model: Comment }]
-    //     })
-    //     res.status(200).json(userData)
-    // } catch (err) {
-    //     res.status(500).json(err);
-    // }
-
-})
-
+    try {
+      const gamesData = await Games.findbyPk(req.params.id, {
+        include: [{
+            model: Review,
+            attributes: ["id", "review_body", "review_date", "user_id"],
+          },
+        ],
+      });
+      if (!gamesData)
+      {
+        res.status(404).json({message: 'No Game found with this id'});
+        return;
+      }
+      res.status(200).json(gamesData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
 
 module.exports = router;
