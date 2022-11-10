2910a9c63b44b492b8bf470730990bddff945e58
Jonathan Moreno <johnfrom209@gmail.com>
2022-11-10 13:26:58 -0800

    seeds for UserData

---
+++ seeds/userData.js
@@ -0,0 +1,28 @@
+const { User } = require('../models');
+
+const userData = [
+    {
+        user_name: "aaa",
+        password: "98765432"
+    },
+    {
+        user_name: "bbb",
+        password: "98765432"
+    },
+    {
+        user_name: "ccc",
+        password: "55555555"
+    },
+    {
+        user_name: "ddd",
+        password: "1223334444"
+    },
+    {
+        user_name: "eee",
+        password: "12345678"
+    },
+]
+
+const seedUsers = () => User.bulkCreate(userData);
+
+module.exports = seedUsers;
