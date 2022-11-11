48794caff868daae8fdfa7df24a34f9b327d9b5d
wayne james <jackmcwilliams@Jacks-MacBook-Air.local>
2022-11-11 15:02:19 -0800

    games.js

---
+++ public/js/game.js
@@ -0,0 +1,6 @@
+const searchFormHandler = async (event) => {
+    event.preventDefault();
+  
+    // Collect values from the login form
+    const search = document.querySelector('#search-form').value.trim();
+};
