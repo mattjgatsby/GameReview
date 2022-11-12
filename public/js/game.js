0b1057b570d06d4b1eab52b9722dd120025bfe8a
Jonathan Moreno <johnfrom209@gmail.com>
2022-11-12 15:53:42 -0800

    added a search function to the api

--- public/js/game.js
+++ public/js/game.js
@@ -1,20 +1,22 @@
-const searchFormHandler = async (event) => {
+const searchForm = async (event) => {
     event.preventDefault();
-  
+
     // Collect values from the login form
     const search = document.querySelector('#form-input').value.trim();
-console.log("search" + search)
-    if(search) {
+    console.log("search" + search)
+    if (search) {
         const response = await fetch('/search', {
-            method: "GET",
-            body: JSON.stringify({search}),
-            headers: {"Content-Type": "application/json"}
+            method: "POST",
+            body: JSON.stringify({ search }),
+            headers: { "Content-Type": "application/json" }
         })
 
-        if(response.ok) {
+        if (response.ok) {
             document.location.replace('/')
         } else {
             alert(response.statusText)
         }
     }
 };
+
+document.querySelector(".searchForm").addEventListener("submit", searchForm);
